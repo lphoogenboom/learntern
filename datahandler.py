b50617cc8a6b55824577569bd1d801b2d603110a
Laurens Hoogenboom <lphoo@Maccy.local>
2024-11-20 18:59:46 +0100

    adds unzipper to datahandler

--- datahandler.py
+++ datahandler.py
@@ -1,10 +1,27 @@
 from pathlib import Path
 import pandas as pd
 import numpy as np
+import zipfile as zf
 
 class Dataloader():
 
-	def csvLoadMNIST(self, csvPath):
+	def unzipMNIST(self, zipPath): # unzip archived files
+		if Path('data/csv/mnist_test.csv').is_file():
+			print('At least 1 unzipped file exist... \nRemove file or change names and try again.')
+			return
+
+		basePath = Path(__file__).resolve().parent # present working directory
+		relativePath = zipPath.split("/") # path from pwd to .zip (including file)
+
+		# append relative path to base path
+		targetPath = basePath
+		for dir in relativePath: 
+			targetPath = targetPath/dir
+
+		with zf.ZipFile(targetPath, 'r') as archive:
+			archive.extractall('data/csv')
+
+	def csvLoadMNIST(self, csvPath): # load both CSVs and merge
 		targetPath = Path(__file__).resolve().parent
 		path = csvPath.split("/")
 		print(path)
@@ -21,7 +38,10 @@ class Dataloader():
 if __name__ == "__main__":
 	print("==== RAN AS FILE ====")
 	loader = Dataloader()
-	images_train, labels_train = loader.csvLoadMNIST('data/raw/mnist_test.csv')
+
+	loader.unzipMNIST('data/raw/mnist.zip')
+
+	# images_train, labels_train = loader.csvLoadMNIST('data/raw/mnist_test.csv')
 
 
 	
